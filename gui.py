7703222ba4228c617495cf709633113be46faefb
Miicrop <103122613+Miicrop@users.noreply.github.com>
2024-06-28 23:27:57 +0200

    new axis move logic

--- gui.py
+++ gui.py
@@ -24,116 +24,142 @@ class App(customtkinter.CTk):
         self.button_frame.grid_columnconfigure((0,1,2), weight=0)
         
         
-        self.button_a1_decrease = customtkinter.CTkButton(self.button_frame, text="-", width=50, command=lambda:self.decrease_theta(0))
+        
+        self.button_a1_decrease = customtkinter.CTkButton(self.button_frame, text="-", width=50)
+        self.button_a2_decrease = customtkinter.CTkButton(self.button_frame, text="-", width=50)
+        self.button_a3_decrease = customtkinter.CTkButton(self.button_frame, text="-", width=50)
+        self.button_a4_decrease = customtkinter.CTkButton(self.button_frame, text="-", width=50)
+        self.button_a5_decrease = customtkinter.CTkButton(self.button_frame, text="-", width=50)
+        self.button_a6_decrease = customtkinter.CTkButton(self.button_frame, text="-", width=50)
         self.button_a1_decrease.grid(row=0, column=0, padx=10, pady=30)
-        
-        self.button_a2_decrease = customtkinter.CTkButton(self.button_frame, text="-", width=50, command=lambda:self.decrease_theta(1))
         self.button_a2_decrease.grid(row=1, column=0, padx=10, pady=30)
-        
-        self.button_a3_decrease = customtkinter.CTkButton(self.button_frame, text="-", width=50, command=lambda:self.decrease_theta(2))
         self.button_a3_decrease.grid(row=2, column=0, padx=10, pady=30)
-        
-        self.button_a4_decrease = customtkinter.CTkButton(self.button_frame, text="-", width=50, command=lambda:self.decrease_theta(3))
         self.button_a4_decrease.grid(row=3, column=0, padx=10, pady=30)
-        
-        self.button_a5_decrease = customtkinter.CTkButton(self.button_frame, text="-", width=50, command=lambda:self.decrease_theta(4))
         self.button_a5_decrease.grid(row=4, column=0, padx=10, pady=30)
-        
-        self.button_a6_decrease = customtkinter.CTkButton(self.button_frame, text="-", width=50, command=lambda:self.decrease_theta(5))
         self.button_a6_decrease.grid(row=5, column=0, padx=10, pady=30)
+        
+        self.button_a1_decrease.bind('<ButtonPress-1>', lambda event: self.control_motor("D", 0))
+        self.button_a2_decrease.bind('<ButtonPress-1>', lambda event: self.control_motor("D", 1))
+        self.button_a3_decrease.bind('<ButtonPress-1>', lambda event: self.control_motor("D", 2))
+        self.button_a4_decrease.bind('<ButtonPress-1>', lambda event: self.control_motor("D", 3))
+        self.button_a5_decrease.bind('<ButtonPress-1>', lambda event: self.control_motor("D", 4))
+        self.button_a6_decrease.bind('<ButtonPress-1>', lambda event: self.control_motor("D", 5))
+        
+        self.button_a1_decrease.bind('<ButtonRelease-1>', lambda event: self.stop_motor(0))
+        self.button_a2_decrease.bind('<ButtonRelease-1>', lambda event: self.stop_motor(1))
+        self.button_a3_decrease.bind('<ButtonRelease-1>', lambda event: self.stop_motor(2))
+        self.button_a4_decrease.bind('<ButtonRelease-1>', lambda event: self.stop_motor(3))
+        self.button_a5_decrease.bind('<ButtonRelease-1>', lambda event: self.stop_motor(4))
+        self.button_a6_decrease.bind('<ButtonRelease-1>', lambda event: self.stop_motor(5))
 
         
-        self.button_a1_increase = customtkinter.CTkButton(self.button_frame, text="+", width=50, command=lambda:self.increase_theta(0))
+        
+        self.button_a1_increase = customtkinter.CTkButton(self.button_frame, text="+", width=50)
+        self.button_a2_increase = customtkinter.CTkButton(self.button_frame, text="+", width=50)
+        self.button_a3_increase = customtkinter.CTkButton(self.button_frame, text="+", width=50)
+        self.button_a4_increase = customtkinter.CTkButton(self.button_frame, text="+", width=50)
+        self.button_a5_increase = customtkinter.CTkButton(self.button_frame, text="+", width=50)
+        self.button_a6_increase = customtkinter.CTkButton(self.button_frame, text="+", width=50)
         self.button_a1_increase.grid(row=0, column=1, padx=10, pady=30)
-        
-        self.button_a2_increase = customtkinter.CTkButton(self.button_frame, text="+", width=50, command=lambda:self.increase_theta(1))
         self.button_a2_increase.grid(row=1, column=1, padx=10, pady=30)
-        
-        self.button_a3_increase = customtkinter.CTkButton(self.button_frame, text="+", width=50, command=lambda:self.increase_theta(2))
         self.button_a3_increase.grid(row=2, column=1, padx=10, pady=30)
-        
-        self.button_a4_increase = customtkinter.CTkButton(self.button_frame, text="+", width=50, command=lambda:self.increase_theta(3))
         self.button_a4_increase.grid(row=3, column=1, padx=10, pady=30)
-        
-        self.button_a5_increase = customtkinter.CTkButton(self.button_frame, text="+", width=50, command=lambda:self.increase_theta(4))
         self.button_a5_increase.grid(row=4, column=1, padx=10, pady=30)
-        
-        self.button_a6_increase = customtkinter.CTkButton(self.button_frame, text="+", width=50, command=lambda:self.increase_theta(5))
         self.button_a6_increase.grid(row=5, column=1, padx=10, pady=30)
+        
+        self.button_a1_increase.bind('<ButtonPress-1>', lambda event: self.control_motor("I", 0))
+        self.button_a2_increase.bind('<ButtonPress-1>', lambda event: self.control_motor("I", 1))
+        self.button_a3_increase.bind('<ButtonPress-1>', lambda event: self.control_motor("I", 2))
+        self.button_a4_increase.bind('<ButtonPress-1>', lambda event: self.control_motor("I", 3))
+        self.button_a5_increase.bind('<ButtonPress-1>', lambda event: self.control_motor("I", 4))
+        self.button_a6_increase.bind('<ButtonPress-1>', lambda event: self.control_motor("I", 5))
+        
+        self.button_a1_increase.bind('<ButtonRelease-1>', lambda event: self.stop_motor(0))
+        self.button_a2_increase.bind('<ButtonRelease-1>', lambda event: self.stop_motor(1))
+        self.button_a3_increase.bind('<ButtonRelease-1>', lambda event: self.stop_motor(2))
+        self.button_a4_increase.bind('<ButtonRelease-1>', lambda event: self.stop_motor(3))
+        self.button_a5_increase.bind('<ButtonRelease-1>', lambda event: self.stop_motor(4))
+        self.button_a6_increase.bind('<ButtonRelease-1>', lambda event: self.stop_motor(5))
+
       
         
         self.label_a1 = customtkinter.CTkLabel(self.button_frame, text="Axis 1")
-        self.label_a1.grid(row=0, column=2, padx=10, pady=30)
-        
         self.label_a2 = customtkinter.CTkLabel(self.button_frame, text="Axis 2")
-        self.label_a2.grid(row=1, column=2, padx=10, pady=30)
-        
         self.label_a3 = customtkinter.CTkLabel(self.button_frame, text="Axis 3")
-        self.label_a3.grid(row=2, column=2, padx=10, pady=30)
-        
         self.label_a4 = customtkinter.CTkLabel(self.button_frame, text="Axis 4")
-        self.label_a4.grid(row=2, column=2, padx=10, pady=30)
-        
-        self.label_a5 = customtkinter.CTkLabel(self.button_frame, text="Axis 5")
-        self.label_a5.grid(row=2, column=2, padx=10, pady=30)
-        
+        self.label_a5 = customtkinter.CTkLabel(self.button_frame, text="Axis 5")      
         self.label_a6 = customtkinter.CTkLabel(self.button_frame, text="Axis 6")
+        self.label_a1.grid(row=0, column=2, padx=10, pady=30)
+        self.label_a2.grid(row=1, column=2, padx=10, pady=30)
+        self.label_a3.grid(row=2, column=2, padx=10, pady=30)
+        self.label_a4.grid(row=2, column=2, padx=10, pady=30)
+        self.label_a5.grid(row=2, column=2, padx=10, pady=30)
         self.label_a6.grid(row=2, column=2, padx=10, pady=30)
         
         
+        
         self.label_a1_theta = customtkinter.CTkLabel(self.button_frame, text="Theta 1")
-        self.label_a1_theta.grid(row=0, column=7, padx=10, pady=30)
-        
         self.label_a2_theta = customtkinter.CTkLabel(self.button_frame, text="Theta 2")
-        self.label_a2_theta.grid(row=1, column=7, padx=10, pady=30)
-        
         self.label_a3_theta = customtkinter.CTkLabel(self.button_frame, text="Theta 3")
-        self.label_a3_theta.grid(row=2, column=7, padx=10, pady=30)
-        
         self.label_a4_theta = customtkinter.CTkLabel(self.button_frame, text="Theta 4")
-        self.label_a4_theta.grid(row=3, column=7, padx=10, pady=30)
-        
         self.label_a5_theta = customtkinter.CTkLabel(self.button_frame, text="Theta 5")
-        self.label_a5_theta.grid(row=4, column=7, padx=10, pady=30)
-        
         self.label_a6_theta = customtkinter.CTkLabel(self.button_frame, text="Theta 6")
+        self.label_a1_theta.grid(row=0, column=7, padx=10, pady=30)
+        self.label_a2_theta.grid(row=1, column=7, padx=10, pady=30)
+        self.label_a3_theta.grid(row=2, column=7, padx=10, pady=30)
+        self.label_a4_theta.grid(row=3, column=7, padx=10, pady=30)
+        self.label_a5_theta.grid(row=4, column=7, padx=10, pady=30)
         self.label_a6_theta.grid(row=5, column=7, padx=10, pady=30)
         
         
+        
         self.button_theta_1 = customtkinter.CTkButton(self.button_frame, text="1", width=50, command=lambda:self.change_theta_constant(1))
-        self.button_theta_1.grid(row=6, column=0, padx=10, pady=(50,10))
-        
         self.button_theta_5 = customtkinter.CTkButton(self.button_frame, text="5", width=50, command=lambda:self.change_theta_constant(5))
-        self.button_theta_5.grid(row=6, column=1, padx=10, pady=(50,10))
-        
         self.button_theta_10 = customtkinter.CTkButton(self.button_frame, text="10", width=50, command=lambda:self.change_theta_constant(10))
+        self.button_theta_1.grid(row=6, column=0, padx=10, pady=(50,10))
+        self.button_theta_5.grid(row=6, column=1, padx=10, pady=(50,10))
         self.button_theta_10.grid(row=6, column=2, padx=10, pady=(50,10))
         
         
+        
         self.button_speed_decrease = customtkinter.CTkButton(self.button_frame, text="-", width=50, command=self.decrease_speed)
-        self.button_speed_decrease.grid(row=0, column=3, padx=10, pady=30)
-        
         self.button_speed_increase = customtkinter.CTkButton(self.button_frame, text="+", width=50, command=self.increase_speed)
-        self.button_speed_increase.grid(row=0, column=4, padx=10, pady=30)
-        
         self.label_speed = customtkinter.CTkLabel(self.button_frame, text="current Speed")
+        self.button_speed_decrease.grid(row=0, column=3, padx=10, pady=30)
+        self.button_speed_increase.grid(row=0, column=4, padx=10, pady=30)
         self.label_speed.grid(row=0, column=5, padx=10, pady=30)
         
         
+        
         self.button_save_current_position = customtkinter.CTkButton(self.button_frame, text="Save current Position", width=200, command=self.save_current_position)
-        self.button_save_current_position.grid(row=0, column=6, padx=10, pady=30)
-        
         self.button_run_programm = customtkinter.CTkButton(self.button_frame, text="Run Programm", width=200, command=self.run_programm)
-        self.button_run_programm.grid(row=1, column=6, padx=10, pady=30)
-        
         self.button_clear_programm = customtkinter.CTkButton(self.button_frame, text="Clear Programm", width=200, command=self.clear_programm)
+        self.button_save_current_position.grid(row=0, column=6, padx=10, pady=30)
+        self.button_run_programm.grid(row=1, column=6, padx=10, pady=30)
         self.button_clear_programm.grid(row=2, column=6, padx=10, pady=30)
         
+        
         self.update_all_on_startup()
+        
+        
         
     def update_all_on_startup(self):
         self.update_speed_label()
         self.update_thetas()
+        
+        
+    def control_motor(self, direction, motor_index):
+        if direction == "D":
+            robot.send_string_over_serial("D", motor_index)
+            robot.get_current_positions()
+            self.update_thetas()
+        if direction == "I":
+            robot.send_string_over_serial("I", motor_index)
+            robot.get_current_positions()
+            self.update_thetas()
+            
+    def stop_motor(self, motor_index):
+        robot.send_string_over_serial("B", motor_index)
         
         
     def decrease_theta(self, axis_index):
@@ -148,9 +174,9 @@ class App(customtkinter.CTk):
         self.label_a1_theta.configure(text=robot.get_theta(0))
         self.label_a2_theta.configure(text=robot.get_theta(1))
         self.label_a3_theta.configure(text=robot.get_theta(2))
-        # self.label_a4_theta.configure(text=robot.get_theta(4))
-        # self.label_a5_theta.configure(text=robot.get_theta(5))
-        # self.label_a6_theta.configure(text=robot.get_theta(6))
+        # self.label_a4_theta.configure(text=robot.get_theta(3))
+        # self.label_a5_theta.configure(text=robot.get_theta(4))
+        # self.label_a6_theta.configure(text=robot.get_theta(5))
         
     def change_theta_constant(self, new_theta_constant):
         self.theta_constant = new_theta_constant
@@ -176,7 +202,6 @@ class App(customtkinter.CTk):
     
     def clear_programm(self):
         robot.clear_thetas()
-        
 
 app = App()
 app.mainloop()
